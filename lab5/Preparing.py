85bc60e42afa0232dc9f676a4733edce34bb275b
Igor Sala <sala-igor@wp.pl>
2025-05-04 15:53:58 +0200

    Dodanie dokumentacji lab5

--- lab5/Preparing.py
+++ lab5/Preparing.py
@@ -1,14 +1,38 @@
+"""
+Moduł zawiera funkcję `createNetwork`, która umożliwia tworzenie grafów warstwowych
+z losowymi krawędziami i wagami, oraz ich wizualizację.
+"""
+
 from Network import Network
 
 def createNetwork(layers):
-    net = Network()
-    net.createLayers(layers)
-    net.addRandomEdges()
-    net.addWeights()
-    # net.draw()
+    """
+    Tworzy graf warstwowy z podaną liczbą warstw, losowymi krawędziami i wagami.
+
+    Parameters
+    ----------
+    layers : int
+        Liczba warstw w grafie.
+
+    Returns
+    -------
+    Network
+        Obiekt klasy Network reprezentujący graf warstwowy.
+    """
+    net = Network()  # Tworzenie obiektu grafu
+    net.createLayers(layers)  # Tworzenie warstw wraz z krawędziami pomiędzy nimi
+    net.addRandomEdges()  # Dodanie losowych krawędzi
+    net.addWeights()  # Dodanie losowych wag do krawędzi
+    # net.draw()  
     return net
 
 
 if __name__ == "__main__":
-    net = createNetwork(2)
-    net.draw()
+    """
+    Przykład użycia modułu Preparing.
+
+    Tworzy graf warstwowy z 2 warstwami, dodaje losowe krawędzie i wagi,
+    a następnie wizualizuje graf.
+    """
+    net = createNetwork(2)  # Tworzenie grafu z 2 warstwami pomiędzy źródłem i ujściem
+    net.draw()  # Wizualizacja grafu
